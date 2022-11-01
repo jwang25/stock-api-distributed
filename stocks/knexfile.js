efe1e6c9e57b2bc841e6cfe00648536cbc6402c7
Ubuntu <ubuntu@ip-172-31-43-91.ap-southeast-2.compute.internal>
2022-11-01 15:20:28 +0000

    modified:   home/Dockerfile 	modified:   home/bin/www 	modified:   stocks/Dockerfile 	modified:   stocks/knexfile.js 	modified:   users/Dockerfile 	modified:   users/bin/www 	modified:   users/knexfile.js

--- stocks/knexfile.js
+++ stocks/knexfile.js
@@ -1,10 +1,10 @@
 module.exports = {
   client: "mysql",
   connection: {
-    host: "localhost",
+    host: "database-1.cxycc90lout0.ap-southeast-2.rds.amazonaws.com",
     database: "webcomputing",
     user: "webapp",
     password: "password",
-    insecureAuth: true,
+    //insecureAuth: true,
   },
 };
